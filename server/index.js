44a8916bb985ad7c8d6938925e1b01b67bcc2b2e
chenjuanhe <chenjuanhe@pptv.com>
2019-10-21 15:44:23 +0800

    安装mysql,koa连接mysql

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 const Koa = require('koa');
+const { normal } = require('./tpl')
+
 const app = new Koa();
 
 app.use((ctx, next) => {
-    ctx.body = '电影首页';
+    ctx.type = 'text/html; charset="utf-8';
+    ctx.body = normal;
 })
 app.listen(4455);
 console.log('listen at 4455')
